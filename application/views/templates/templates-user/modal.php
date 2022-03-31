a7e71a3e7f7fa249e135daf8d91341c4f4faaa66
Riyan First Tiyanto <riyanfirst13@gmail.com>
2022-03-31 21:49:21 +0700

    menambah modals

---
+++ application/views/templates/templates-user/modal.php
@@ -0,0 +1,13 @@
+<!-- login modals -->
+<div class="modal fade" tabindex="1" id="loginModal" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal">
+                <h5 class="modal-title">Login Member</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
